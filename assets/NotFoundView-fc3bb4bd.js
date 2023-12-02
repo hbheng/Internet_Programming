98708de727399de5fc849baf28e3e02d1c3795da
hbheng <hb988651@gmail.com>
2023-12-02 17:06:11 +0700

    dist

---
+++ assets/NotFoundView-fc3bb4bd.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as t,a as n}from"./index-e826914a.js";const s={name:"notFoundView"},c=n("h1",null,"Not Found 404",-1),a=[c];function r(_,d,i,u,l,p){return e(),t("div",null,a)}const m=o(s,[["render",r]]);export{m as default};
